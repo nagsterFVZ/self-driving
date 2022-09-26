39c72e3f7ada985d9dae896e64f0eca8bdffe7f1
nagsterFVZ <nagsterza@gmail.com>
2022-09-26 12:41:39 +0200

    camera working

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ from picamera2 import Picamera2
 from libcamera import Transform
 import cv2
 import time
-#Initialize the Flask app
-app = Flask(__name__)
 
 picam2 = Picamera2()
-camera_config = picam2.create_video_configuration(main={"size": (1280, 720)}, transform=Transform(vflip=1))
+camera_config = picam2.create_video_configuration(main={"format": 'XRGB8888', "size": (1280, 720)}, transform=Transform(vflip=0))
 picam2.configure(camera_config)
 picam2.start()
-time.sleep(2)
-im = picam2.capture_array()
+time.sleep(2.0)
+
+app = Flask(__name__)
 
 def gen_frames():  
     while True:
