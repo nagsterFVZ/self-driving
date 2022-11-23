f481c1122f177db93e488bb888738fc6a7a45681
nagsterFVZ <nasgterza@gmail.com>
2022-11-23 12:17:10 +0100

    Changes on Pi

--- start.bash
+++ start.bash
@@ -1,7 +1,7 @@
 echo "Starting Redis Server"
-sudo /etc/init.d/redis-server start
+sudo systemctl restart redis-server
 echo "Starting ingest and flask app"
 pm2 start ecosystem.config.js
-echo "Restarting Nginx"
-sudo systemctl restart nginx
+#echo "Restarting Nginx"
+#sudo systemctl restart nginx
 echo "Startup completed"
